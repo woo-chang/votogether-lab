3e1627acd17fdb3b8fb01da018a1a1a74c8ce000
woo-chang <clllickme@naver.com>
2023-07-11 22:07:40 +0900

    feat: Init Project

---
+++ src/test/java/com/example/votogether/VotogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.votogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
