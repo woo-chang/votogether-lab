65790656a2c7a00d6e03183739a0955c111731bd
woo-chang <clllickme@naver.com>
2023-07-12 11:39:27 +0900

    feat: 테스트 서비스 기능 구현

---
+++ src/main/java/com/example/votogether/test/TestService.java
@@ -0,0 +1,12 @@
+package com.example.votogether.test;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public String test() {
+        return "test";
+    }
+
+}
