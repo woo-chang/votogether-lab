87d111f0f021dc84478ddf34d85aec1c834619cf
woo-chang <clllickme@naver.com>
2023-08-03 11:16:02 +0900

    refactor: 경로 저장 로직 리팩토링

--- src/main/java/com/example/votogether/image/FileService.java
+++ src/main/java/com/example/votogether/image/FileService.java
@@ -13,13 +13,16 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FileService {
 
-    private static final String ROOT_PATH = "images";
-    private static final String RESOURCE_PATH =
-            System.getProperty("user.dir") + File.separator + ROOT_PATH + File.separator;
+    private static final String ROOT_PATH = "static";
+    private static final String IMAGE_PATH = "images";
+    private static final String IMAGE_FULL_PATH =
+            System.getProperty("user.dir") + File.separator + ROOT_PATH + File.separator + IMAGE_PATH + File.separator;
+    private static final String IMAGE_REQUEST_PATH =
+            ROOT_PATH + File.separator + IMAGE_PATH + File.separator;
 
     static {
         try {
-            Files.createDirectories(Paths.get(RESOURCE_PATH));
+            Files.createDirectories(Paths.get(IMAGE_FULL_PATH));
         } catch (IOException ignore) {
             throw new IllegalStateException("디렉토리 생성 도중 에러가 발생하였습니다.");
         }
@@ -28,17 +31,22 @@ public class FileService {
     public String save(MultipartFile image) {
         try {
             String fileFullPath = generateFileFullPath(image);
+            String filePath = generateFilePath(image);
             validateFilePathLength(fileFullPath);
             validateImageFile(image);
             image.transferTo(new File(fileFullPath));
-            return fileFullPath;
+            return filePath;
         } catch (IOException e) {
             throw new IllegalStateException("사진 저장에 실패하였습니다.");
         }
     }
 
     private String generateFileFullPath(MultipartFile image) {
-        return RESOURCE_PATH + image.getOriginalFilename();
+        return IMAGE_FULL_PATH + image.getOriginalFilename();
+    }
+
+    private String generateFilePath(MultipartFile image) {
+        return IMAGE_REQUEST_PATH + image.getOriginalFilename();
     }
 
     private void validateFilePathLength(String fileFullPath) {
