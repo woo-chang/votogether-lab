dd1f560f5f211328115f8f37c26830cb11c59d42
woo-chang <clllickme@naver.com>
2023-08-14 04:02:30 +0900

    feat: 1분당 로깅

--- src/main/java/com/example/votogether/LogController.java
+++ src/main/java/com/example/votogether/LogController.java
@@ -1,8 +1,6 @@
 package com.example.votogether;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LogController {
 
-    private final Logger log = LogManager.getLogger(LogController.class);
     private final LogService logService;
 
     @GetMapping("/log")
     public String log() {
-        log.trace("trace-log");
-        log.debug("debug-log");
-        log.info("info-log");
-        log.warn("warn-log");
-        log.error("error-log");
-        return "log";
+        return logService.log();
     }
 }
--- src/main/java/com/example/votogether/LogService.java
+++ src/main/java/com/example/votogether/LogService.java
@@ -1,7 +1,20 @@
 package com.example.votogether;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 public class LogService {
+
+    @Transactional(readOnly = true)
+    public String log() {
+        log.trace("trace-log");
+        log.debug("debug-log");
+        log.info("info-log");
+        log.warn("warn-log");
+        log.error("error-log");
+        return "log";
+    }
 }
